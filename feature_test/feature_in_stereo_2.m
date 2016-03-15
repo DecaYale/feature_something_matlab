2774b696305db5fa679e82f260eacf236878dfce
DecaYale <deca-yale@foxmail.com>
2016-03-15 16:08:51 +0800

    program is not finished in feature_in_stereo_2.m

---
+++ feature_test/feature_in_stereo_2.m
@@ -0,0 +1,125 @@
+clc
+clear
+close all;
+imgL =imread('data\scene1.row3.col3.ppm');% imread('data\test.jpg');%imread('data\scene1.row3.col3.ppm');
+imgL = rgb2gray(imgL);
+
+imgR =imread('data\scene1.row3.col4.ppm');% imread('data\test.jpg');%imread('data\scene1.row3.col3.ppm');
+imgR = rgb2gray(imgR);
+
+
+
+s = 1;  %步长
+% 计算x,y方向梯度
+[pxL,pyL] = gradient_0(imgL,s);
+[pxR,pyR] = gradient_0(imgR,s);
+%Magnitude_xy = sqrt(pxL.^2+py.^2);
+MagThresh = 20;
+SimThresh = 0.7*MagThresh;
+xDiffThresh = 40;
+
+%% 提取特征点
+fea_xL = [];
+fea_yL = [];
+for i=1:size(pxL,1)
+    for j=1:size(pxL,2)
+        %if (Magnitude_xy(i,j)> MagThresh  ...
+           %    && abs(px(i,j)- py(i,j))<SimThresh )
+        if (abs(pxL(i,j)) > xDiffThresh)
+            fea_xL = [fea_xL,j];
+            fea_yL = [fea_yL,i];
+        end
+    end
+end
+imshow(imgL);
+hold on;
+plot(fea_xL,fea_yL,'r.');
+
+fea_xR = [];
+fea_yR = [];
+for i=1:size(pxR,1)
+    for j=1:size(pxR,2)
+        %if (Magnitude_xy(i,j)> MagThresh  ...
+           %    && abs(px(i,j)- py(i,j))<SimThresh )
+        if (abs(pxR(i,j)) > xDiffThresh)
+            fea_xR = [fea_xR,j];
+            fea_yR = [fea_yR,i];
+        end
+    end
+end
+figure;
+imshow(imgR);
+hold on;
+plot(fea_xR,fea_yR,'r.');
+
+
+%% matching
+%邻接表建立
+imgH = size(imgL,1);
+imgW = size(imgL,2);
+
+fea_list_L = cell(length(fea_xL), 1);
+for i=1:length(fea_xL)
+    x = fea_xL(i);
+    y = fea_yL(i);
+    fea_list_L{y} = [fea_list_L{y},x];
+end
+
+fea_list_R = cell(length(fea_xR), 1);
+for i=1:length(fea_xR)
+    x = fea_xR(i);
+    y = fea_yR(i);
+    fea_list_R{y} = [fea_list_R{y},x];
+end
+
+
+
+%
+winSize = 3; % odd 
+halfWinSize = int32(winSize) / 2 ;
+pair = [];
+for y = 1:size(fea_list_L,1)
+    
+     if (y<=halfWinSize || y>imgH - halfWinSize || x<=halfWinSize || x >imgW - halfWinSize ) 
+          continue;
+     end
+           
+   for i = 1:length(fea_list_L{y}) 
+       xL = fea_list_L{y}(i);
+       for j = 1:length(fea_list_R{y})
+           xR= fea_list_R{y}(j);
+          
+           SAD = imgL(y-halfWinSize:y+halfWinSize,xL-halfWinSize:xL+halfWinSize) - ...
+                    imgR(y-halfWinSize:y+halfWinSize,xR-halfWinSize:xR+halfWinSize);
+                
+           SAD = sum(sum( abs(SAD) ));
+            
+           dist(i,j) = SAD;
+           
+          %{ 
+           SAD = 0;
+           for win_i = 1:winSize
+               for win_j = 1:winSize
+                   x_r = xR + win_j - int(winSize)*0.5;
+                   y = y  + win_i - int(WinSize)*0.5;
+                   x_l = xL + win_j - int(winSize)*0.5;
+                   
+                   SAD = SAD + imgL
+                     dist(i,j) = 
+               end
+           end
+           %}
+       end
+   end
+    % 排序，并且选出交叉验证
+        [L_R,I_lr] = sort(dist,2);
+        [R_L,I_rl] = sort(dist,1);
+        for i = 1:1:length(fea_list_L{y})
+            if ( I_rl(1,I_lr(i,1)) == fea_list_L{y}(i) )  %%%%
+               pair = [pair; fea_list_L{y}(i),y, fea_list_R{y}(I_lr(i,1)), y];
+            end
+        end
+        
+ end
+    
+
